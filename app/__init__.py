59fced2f12a476954d5aa0b8faddbb2279db4725
sakzou <sakuoz@163.com>
2016-12-28 17:40:23 +0800

    完成表单验证和数据库配置

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)                             # 创建对象(数据库)
 
-from app import views
+from app import views, models                        # 导入模块
--- config.py
+++ config.py
@@ -1,2 +1,13 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-gusess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }
+]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')    # 数据库路径
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')       # 存储数据文件的文件夹
