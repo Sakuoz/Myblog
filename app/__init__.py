a6064b75398ab2e34174959de3da5ddeb690a7c7
sakuoz <sakuoz@163.com>
2017-01-02 22:58:14 +0800

    完成用户登录系统

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,13 @@ app.config.from_object('config')
 db = SQLAlchemy(app)                             # 创建对象(数据库)
 
 from app import views, models                        # 导入模块
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
--- app/views.py
+++ app/views.py
@@ -1,12 +1,15 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
+from .models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'Sakura'} # fake user
-    posts = [ # fake array of posts
+    user = g.user
+    posts = [
         {
             'author': {'nickname': 'Sakura'},
             'body': 'Beautiful day in Portland!'
@@ -28,15 +31,48 @@ def index():
 # index view function suppressed for brevity
 
 @app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))        # 如果已经登陆过,会直接重定向首页,不需要二次登录
     form = LoginForm()
     if form.validate_on_submit():
-        flash(' Login requested for 我的ID="'
-            + form.openid.data
-            + '", remember_me= '
-            + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
     return render_template('login.html',
         title = 'Sign In',
         form = form,
         providers = app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+        if resp.email is None or resp.email == "":
+            flash('Invalid login. Please try again.')
+            return redirect(url_for('login'))
+        user = User.query.filter_by(email = resp.email).first()
+        if user is None:
+            nickname = resp.nickname
+            if nickname is None or nickname == "":
+                nickname = resp.email.split('@')[0]
+            user = User(nickname = nickname, email = resp.email)
+            db.session.add(user)
+            db.session.commit()
+        remember_me = False
+        if 'remember_me' in session:
+            remember_me = session['remember_me']
+            session.pop('remember_me', None)
+        login_user(user, remember = remember_me)
+        return redirect(request.args.get('next') or url_for('index'))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))     # id永远为unicode字符串,因此id发送之前需要转成整型,否则报错
