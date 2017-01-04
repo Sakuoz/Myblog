a3888e04528bd9f8dd3e9bfecc667ea405f4029a
sakzou <sakuoz@163.com>
2017-01-04 17:38:33 +0800

    完成用户头像和时间显示

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from app import db
+import hashlib
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     def is_authenticated(self):
         return True
@@ -17,6 +20,10 @@ class User(db.Model):
 
     def get_id(self):
         return str(self.id)          # python3
+
+    def avatar(self, size):
+        m = hashlib.md5(self.email.encode(encoding='utf-8'))
+        return 'http://www.gravatar.com/avatar/' + m.hexdigest() + '?d=mm&s=' + str(size)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
